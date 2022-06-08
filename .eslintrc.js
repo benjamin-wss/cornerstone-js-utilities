941de8331fb922760151acf69546679d8fc845dc
Benjamin Wong <benjamin.wss@gmail.com>
2022-06-08 21:32:50 +0800

    added prettier plugin into eslint config :boom:

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     es2021: true,
   },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: "latest",
   },
