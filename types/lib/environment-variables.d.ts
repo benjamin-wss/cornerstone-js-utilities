3398a3a347fde5f5f43d17b7662eebac6bf52280
Benjamin Wong <benjamin.wss@gmail.com>
2022-08-20 23:45:47 +0800

    updated types

--- types/lib/environment-variables.d.ts
+++ types/lib/environment-variables.d.ts
@@ -12,7 +12,7 @@ export function getEnvVariableAsString({ fieldName, defaultValue }: {
 }): string;
 /**
  * Gets environment variable as integer.
- *
+ * If default value is provided but no value is provided, default value is used.
  * If no default value is provided and no value is provided, an error will be thrown.
  * If default value and/or value passed in is not an integer, an error will be thrown.
  * @param {object} param0 Input parameters.
@@ -26,15 +26,36 @@ export function getEnvVariableAsInteger({ fieldName, defaultValue, minValue }: {
     defaultValue?: number;
     minValue?: number;
 }): number;
+/**
+ * Gets environment variable as boolean.
+ * If default value is provided but no value is provided, default value is used.
+ * If no default value is provided and no value is provided, an error will be thrown.
+ * If default value and/or value passed in is not an boolean, an error will be thrown.
+ * @param {object} param0 Input parameters.
+ * @param {string} param0.fieldName Environment variable field name.
+ * @param {number} [param0.defaultValue] Environment variable field default value.
+ * @returns {number}
+ */
 export function getEnvVariableAsBoolean({ fieldName, defaultValue }: {
-    fieldName: any;
-    defaultValue?: boolean;
-}): boolean;
+    fieldName: string;
+    defaultValue?: number;
+}): number;
+/**
+ * Gets environment variable as decimal.
+ * If default value is provided but no value is provided, default value is used.
+ * If no default value is provided and no value is provided, an error will be thrown.
+ * If default value and/or value passed in is not an decimal, an error will be thrown.
+ * @param {object} param0 Input parameters.
+ * @param {string} param0.fieldName Environment variable field name.
+ * @param {number} [param0.defaultValue] Environment variable field default value.
+ * @param {number} [param0.minValue=0] Environment variable field default value.
+ * @returns {number}
+ */
 export function getEnvVariableAsDecimal({ fieldName, defaultValue, minValue }: {
-    fieldName: any;
-    defaultValue: any;
+    fieldName: string;
+    defaultValue?: number;
     minValue?: number;
-}): any;
+}): number;
 export function getEnvVariableAsDateString({ fieldName, defaultValue, dateFormatMask, }: {
     fieldName: any;
     defaultValue: any;
