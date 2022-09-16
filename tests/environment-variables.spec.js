7fc0eadf18462a6512eb0c35e7e701cedd73cc3c
Benjamin Wong <benjamin.wss@gmail.com>
2022-09-17 01:30:49 +0800

    added more unit tests

--- tests/environment-variables.spec.js
+++ tests/environment-variables.spec.js
@@ -51,5 +51,55 @@ describe("EnvironmentVariables", () => {
         `The environment variable ${fieldName} does not have a default value and is null, please provide a value or define a default value.`
       );
     });
+    test("A default value is set and should return the default value if no env var is supplied", () => {
+      const value = EnvironmentVariables.getEnvVariableAsString({
+        fieldName: "TEST_STRING",
+        defaultValue: "TEST_STRING_VALUE",
+      });
+      expect(value).toBe("TEST_STRING_VALUE");
+    });
+    test("A default value is set and value is supplied via env var as an override, it should reflect the override.", () => {
+      const value = EnvironmentVariables.getEnvVariableAsString({
+        fieldName: "ENV_VAR_STRING_OVERRIDE_TEST",
+        defaultValue: "TEST_STRING_VALUE",
+      });
+      expect(value).not.toBe("TEST_STRING_VALUE");
+      expect(value).toBe(process.env.ENV_VAR_STRING_OVERRIDE_TEST);
+    });
+    test("If a valid value that matches the list in allowedValues, it is allowed", () => {
+      const value = process.env.ALLOWED_VALUE_TEST_STRING;
+      const allowedValues = ["foo", "bar"];
+
+      const testCase = () => {
+        return EnvironmentVariables.getEnvVariableAsString({
+          fieldName: "ALLOWED_VALUE_TEST_STRING",
+          allowedValues,
+        });
+      };
+
+      expect(() => {
+        testCase();
+      }).not.toThrow(
+        `Supplied value (${value}) is not allowed. Allowed values are [${allowedValues.join(
+          ","
+        )}].`
+      );
+
+      expect(testCase()).toEqual(value);
+    });
+    test("If an invalid value that matches the list in allowedValues, it throws", () => {
+      const value = process.env.ALLOWED_VALUE_NEGATIVE_TEST_STRING;
+      const allowedValues = ["foo", "bar"];
+      expect(() => {
+        EnvironmentVariables.getEnvVariableAsString({
+          fieldName: "ALLOWED_VALUE_NEGATIVE_TEST_STRING",
+          allowedValues,
+        });
+      }).toThrow(
+        `Supplied value (${value}) is not allowed. Allowed values are [${allowedValues.join(
+          ","
+        )}].`
+      );
+    });
   });
 });
